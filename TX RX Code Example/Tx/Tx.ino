40201d9f36a45d9f6bb0ff6efe3cc4ce0f163532
Manash Kumar Mandal <manash@protonmail.com>
2016-03-29 11:06:07 +0600

    Update Tx.ino

--- TX RX Code Example/Tx/Tx.ino
+++ TX RX Code Example/Tx/Tx.ino
@@ -1,25 +1,32 @@
 //simple Tx on pin D12
-//..................................
+// Manash
+#define LED 13
+#define TX_PIN 12
 #include <VirtualWire.h>
 
 char *controller;
 
 void setup() {
-  pinMode(13,OUTPUT);
-  vw_set_ptt_inverted(true); //
-  vw_set_tx_pin(12);
+  pinMode(LED, OUTPUT);
+  
+  vw_set_ptt_inverted(true); 
+  vw_set_tx_pin(TX_PIN);
+  
   vw_setup(4000);// speed of data transfer Kbps
 }
 
 void loop(){
-  controller="1"  ;
+  controller= "1";
   vw_send((uint8_t *)controller, strlen(controller));
   vw_wait_tx(); // Wait until the whole message is gone
+  
   digitalWrite(13,1);
   delay(2000);
-  controller="0"  ;
+  controller="0";
+  
   vw_send((uint8_t *)controller, strlen(controller));
   vw_wait_tx(); // Wait until the whole message is gone
-  digitalWrite(13,0);
+  
+  digitalWrite(LED, LOW);
   delay(2000);
 }
