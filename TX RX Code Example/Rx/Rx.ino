ef5a82f7b8b81425110b0959601a745f13fe14e1
Manash Kumar Mandal <manash@protonmail.com>
2016-03-29 11:03:47 +0600

    fixed code typo

--- TX RX Code Example/Rx/Rx.ino
+++ TX RX Code Example/Rx/Rx.ino
@@ -1,5 +1,5 @@
 //Written By : Manash
-//..................................
+
 #include <VirtualWire.h>
 void setup()
 {
@@ -7,24 +7,17 @@ void setup()
     vw_set_rx_pin(12);
     vw_setup(4000);  // Bits per sec
     pinMode(13, OUTPUT);
-
     vw_rx_start();       // Start the receiver PLL running
 }
-    void loop()
+
+void loop()
 {
     uint8_t buf[VW_MAX_MESSAGE_LEN];
     uint8_t buflen = VW_MAX_MESSAGE_LEN;
-
+    
     if (vw_get_message(buf, &buflen)) // Non-blocking
     {
-      if(buf[0]=='1'){
-
-  
-   digitalWrite(13,1);
-      }  
-   if(buf[0]=='0'){
-  digitalWrite(13,0);
-    }
-
-}
+      if(buf[0]=='1') digitalWrite(13,1);
+      if(buf[0]=='0') digitalWrite(13,0);
+    }   
 }
